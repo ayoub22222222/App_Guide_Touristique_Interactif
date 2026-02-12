3aa6f5240e1cb8acc122506caf74454b52c3adc6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2026-02-12 23:27:37 +0100

    new update

--- frontend/my-app/src/components/common/ProductCard.jsx
+++ frontend/my-app/src/components/common/ProductCard.jsx
@@ -1,19 +1,35 @@
 import React from "react";
+import Italy from "../../assets/Italy.jpg";
 
 
 
 export default function ProductCard() {
     return (
-         <div class="w-60 p-2 bg-gray-300 rounded-xl ">
-    {/* <img src="#" alt="" class="h-40 object-cover"> */}
-    <div>
-      <h2>Hello Kitty</h2>
+    //      <div className="w-60 p-2 bg-gray-300 rounded-xl ">
+    // <img src={Italy} alt="hero" className="h-40 object-cover" />
+    // <div>
+    //   <h2>Hello Kitty</h2>
     
-      <p>random text tohelp me understand</p>
-      <div>
-        <a href="" role="button">learn More</a>
+    //   <p>random text tohelp me understand</p>
+    //   <div>
+    //     <a href="" role="button">learn More</a>
+    //   </div>
+    // </div>
+
+    <div className="w-60 p-2 bg-gray-300 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl ">
+  
+    <img src={Italy} alt="" className="h-40 object-cover rounded-xl" />
+    <div>
+      <h2 className="font-bold text-lg">Hello Kitty</h2>
+      
+      <p className="text-sm ">random text tohelp me understand
+        random text tohelp me understand
+      </p>
+      <div className="m-2">
+        <a href="" role="button" class="text-white bg-black px-3 py-1 rounded-md hover:bg-secondary">learn More</a>
       </div>
     </div>
   </div>
+  
     )
 }
